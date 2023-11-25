c67be359a5dd30ce7d56bf4c3316d4a0c3c00fc1
dasdgw <michifrank2@gmx.de>
2023-11-25 18:40:16 +0100

    revert VideoGeneric and add dvi module with rgb888 & rgb565support

--- litex/soc/cores/video.py
+++ litex/soc/cores/video.py
@@ -748,7 +748,49 @@ class VideoFrameBuffer(LiteXModule):
 # Generic (Very Generic PHY supporting VGA/DVI and variations).
 
 class VideoGenericPHY(LiteXModule):
-    def __init__(self, pads, clock_domain="sys", with_clk_ddr_output=True, output_mode="default", clk_clock_domain="sys"):
+    def __init__(self, pads, clock_domain="sys", with_clk_ddr_output=True):
+        self.sink = sink = stream.Endpoint(video_data_layout)
+
+        # # #
+
+        # Always ack Sink, no backpressure.
+        self.comb += sink.ready.eq(1)
+
+        # Drive Clk.
+        if hasattr(pads, "clk"):
+            if with_clk_ddr_output:
+                self.specials += DDROutput(i1=1, i2=0, o=pads.clk, clk=ClockSignal(clock_domain))
+            else:
+                self.comb += pads.clk.eq(ClockSignal(clock_domain))
+
+        # Drive Controls.
+        if hasattr(pads, "de"):
+            self.specials += SDROutput(i=sink.de, o=pads.de, clk=ClockSignal(clock_domain))
+        if hasattr(pads, "hsync_n") and hasattr(pads, "vsync_n"):
+            self.specials += SDROutput(i=~sink.hsync, o=pads.hsync_n, clk=ClockSignal(clock_domain))
+            self.specials += SDROutput(i=~sink.vsync, o=pads.vsync_n, clk=ClockSignal(clock_domain))
+        else:
+            self.specials += SDROutput(i=sink.hsync,  o=pads.hsync,   clk=ClockSignal(clock_domain))
+            self.specials += SDROutput(i=sink.vsync,  o=pads.vsync,   clk=ClockSignal(clock_domain))
+
+        # Drive Datas.
+        cbits  = len(pads.r)
+        cshift = (8 - cbits)
+        for i in range(cbits):
+            # VGA monitors interpret minimum value as black so ensure data is set to 0 during blanking.
+            self.specials += SDROutput(i=sink.r[cshift + i] & sink.de, o=pads.r[i], clk=ClockSignal(clock_domain))
+            self.specials += SDROutput(i=sink.g[cshift + i] & sink.de, o=pads.g[i], clk=ClockSignal(clock_domain))
+            self.specials += SDROutput(i=sink.b[cshift + i] & sink.de, o=pads.b[i], clk=ClockSignal(clock_domain))
+
+
+# VGA (Generic).
+
+class VideoVGAPHY(VideoGenericPHY): pass
+
+# DVI (Generic).
+
+class VideoDVIPHY(LiteXModule):
+    def __init__(self, pads, clock_domain="sys", with_clk_ddr_output=True, format="rgb565", clk_clock_domain="sys"):
         self.sink = sink = stream.Endpoint(video_data_layout)
 
         # # #
@@ -777,34 +819,25 @@ class VideoGenericPHY(LiteXModule):
             self.specials += SDROutput(i=sink.hsync,  o=pads.hsync,   clk=ClockSignal(clock_domain))
             self.specials += SDROutput(i=sink.vsync,  o=pads.vsync,   clk=ClockSignal(clock_domain))
 
-        # Drive Datas.
-        if (output_mode == "default"):
-            cbits  = len(pads.r)
-            cshift = (8 - cbits)
-            for i in range(cbits):
-                # VGA monitors interpret minimum value as black so ensure data is set to 0 during blanking.
-                self.specials += SDROutput(i=sink.r[cshift + i] & sink.de, o=pads.r[i], clk=ClockSignal(clock_domain))
-                self.specials += SDROutput(i=sink.g[cshift + i] & sink.de, o=pads.g[i], clk=ClockSignal(clock_domain))
-                self.specials += SDROutput(i=sink.b[cshift + i] & sink.de, o=pads.b[i], clk=ClockSignal(clock_domain))
-
-        # 12bit ddr mode
-        if (output_mode ==  "chrontel_idf_0"):
+        # 12bit ddr mode rgb888 chrontel_idf_0
+        if (format == "rgb888"):
             cbits  = 4
             for i in range(cbits):
-                # VGA monitors interpret minimum value as black so ensure data is set to 0 during blanking.
                 self.specials += DDROutput(i1=sink.g[i] & sink.de, i2=sink.r[4 + i] & sink.de, o=pads.d[8 + i], clk=ClockSignal(clock_domain))
                 self.specials += DDROutput(i1=sink.b[4 + i] & sink.de, i2=sink.r[i] & sink.de, o=pads.d[4 + i], clk=ClockSignal(clock_domain))
                 self.specials += DDROutput(i1=sink.b[i] & sink.de, i2=sink.g[4 + i] & sink.de, o=pads.d[i], clk=ClockSignal(clock_domain))
 
-
-
-# VGA (Generic).
-
-class VideoVGAPHY(VideoGenericPHY): pass
-
-# DVI (Generic).
-
-class VideoDVIPHY(VideoGenericPHY): pass
+        # 8bit ddr mode rgb565 chrontel_idf_2
+        if (format == "rgb565"):
+            cbits  = 3
+            for i in range(2, 2 + cbits):
+                self.specials += DDROutput(i1=sink.g[i] & sink.de, i2=sink.r[3 + i] & sink.de, o=pads.d[7 + i], clk=ClockSignal(clock_domain))
+                self.specials += DDROutput(i1=sink.b[1 + i] & sink.de, i2=sink.g[3 + i] & sink.de, o=pads.d[2 + i], clk=ClockSignal(clock_domain))
+            for i in range(3, 5):
+                self.specials += DDROutput(i1=sink.b[3 + i] & sink.de, i2=sink.r[i] & sink.de, o=pads.d[4 + i], clk=ClockSignal(clock_domain))
+            # todo assign 0 to pads.d[0:4], no clk need
+            for i in range(4):
+                self.specials += DDROutput(i1=0 & sink.de, i2=0 & sink.de, o=pads.d[i], clk=ClockSignal(clock_domain))
 
 # HDMI (Generic).
 
